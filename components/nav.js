5dd986a95b4ba6f632ad1c416cb1aa51441d2f07
DerekRdgz <derek_rdgz@outlook.com>
2025-05-09 23:10:27 -0600

    feat: :sparkles: Successful connection to database!

---
+++ components/nav.js
@@ -0,0 +1,29 @@
+(async () => {
+  // 1. Cargar el fragmento y meterlo en el contenedor #navbar
+  const resp = await fetch("components/navbar.html");
+  const html = await resp.text();
+  document.getElementById("navbar").innerHTML = html;
+
+  // 2. Lógica de sesión (se ejecuta DESPUÉS de que la barra ya existe)
+  const sessionLi = document.getElementById("sessionArea");
+  const token = localStorage.getItem("token");
+  const nameLS = localStorage.getItem("name");
+
+  if (token && nameLS) {
+    sessionLi.innerHTML = `
+        <span id="profileBtn" class="navbar-text me-3 fw-semibold" style="cursor: pointer">Hola, ${nameLS}</span>
+        <button id="logoutBtn" class="btn btn-outline-secondary btn-sm">Cerrar sesión</button>
+      `;
+    document.getElementById("logoutBtn").onclick = () => {
+      localStorage.clear();
+      location.href = "login.html";
+    };
+    document.getElementById("profileBtn").onclick = () => {
+      location.href = "profile.html";
+    };
+  } else {
+    sessionLi.innerHTML = `
+        <a class="btn btn-sm text-whit" href="login.html" style="background-color: #123333">Inicia sesión</a>
+      `;
+  }
+})();
